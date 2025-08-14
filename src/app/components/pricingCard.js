246790f0f577c8650d613085c2c5dc08aa7e936a
Ali07117 <alirashid07117@gmail.com>
2025-08-14 06:07:30 +0500

    update

--- src/app/components/pricingCard.js
+++ src/app/components/pricingCard.js
@@ -1,41 +1,57 @@
 "use client"
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PricingOption from './pricingOption';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 
 function PricingCard() {
+  
+
     var settings = {
         infinite: false,
         speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
+        slidesToShow: 3,
+        slidesToScroll: 3,
         arrows: false,
         responsive: [
           {
             breakpoint: 2500,
             settings: {
               slidesToShow: 3,
-              slidesToScroll: 1,
+              slidesToScroll: 3,
             },
           },
           {
             breakpoint: 1024,
             settings: {
               slidesToShow: 2,
-              slidesToScroll: 1,
+              slidesToScroll: 2,
             },
           },
           {
             breakpoint: 950,
             settings: {
               slidesToShow: 2,
-              slidesToScroll: 1,
+              slidesToScroll: 2,
             },
           },
           {
             breakpoint: 721,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            },
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            },
+          },
+          {
+            breakpoint: 420,
             settings: {
               slidesToShow: 1,
               slidesToScroll: 1,
